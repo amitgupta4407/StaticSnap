8d0aa834a7ede20d9b6fcc2ea9fc9ed0476a4636
Amit Gupta <amitgupta4407@gmail.com>
2025-03-07 23:39:40 +0530

    init proj

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import glob
+from fastapi import FastAPI,  Response
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+STATIC_FOLDER = "static"
+
+app.mount("/static", StaticFiles(directory=STATIC_FOLDER), name="static")
+
+@app.get("/img_cnt")
+async def image_count():
+    image_files = glob.glob(os.path.join(STATIC_FOLDER, '*.*'))
+    return {'count': len(image_files)}
+
+@app.get("/images")
+async def get_images():
+    image_files = glob.glob(os.path.join(STATIC_FOLDER, '*.*'))
+    image_names = [os.path.basename(img) for img in image_files]
+    return image_names
+
+@app.get("/static/{filename}")
+async def serve_static(filename: str):
+    path = os.path.join(STATIC_FOLDER, filename)
+    if os.path.exists(path):
+        return FileResponse(path)
+    else:
+        return Response(status_code=404)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
